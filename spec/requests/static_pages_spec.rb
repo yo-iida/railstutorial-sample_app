2b3d6931a74e5a8fd95da03bd485a65d35011e00
Yoshiki Iida <yoshiki.iida@crowdworks.co.jp>
2015-05-20 16:19:13 +0900

    [add]contactテスト

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -47,5 +47,18 @@ describe "Static pages" do
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
+  
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 
 end
