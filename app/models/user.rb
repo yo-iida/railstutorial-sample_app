7171b6be29806e1ff5e0858c86b7665231e87bed
Yoshiki Iida <yoshiki.iida@crowdworks.co.jp>
2015-05-20 20:59:33 +0900

    [modify]演習

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
+  has_secure_password
+
+  #メールアドレスバリデーション正規表現
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
   #モデルのバリデーションを追加
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
-  has_secure_password
 
   #パスワードの存在証明がhas_secure_passwordで動かなかったので、presenceも追加している
   validates :password,  presence: true, length: { minimum: 6 }
