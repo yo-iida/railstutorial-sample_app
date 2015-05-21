1730f6edf2995d1aa6ecb089eb7993a1a4e422f1
Yoshiki Iida <yoshiki.iida@crowdworks.co.jp>
2015-05-21 20:25:02 +0900

    [modify]サインインサインアウト機能実装

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,8 +40,18 @@ describe "User pages" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
+      describe "followed by signout" do
+        before do
+          click_button submit
+          click_link "Sign out"
+        end
+
+        it { should have_link('Sign in') }
       end
 
     end
